182f18089cd05337366591c8cfadbc787b1ac4fb
Hemant Nag <104633997+dev-hemant63@users.noreply.github.com>
2024-08-12 10:46:23 +0530

    Add project files.

---
+++ VMart.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VMart.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
